7e4754f027b9b9bc47ece8af9701ce3864a9b76f
Mahmoud Ahmed <wa7wa776@gmail.com>
2023-02-13 19:08:25 +0200

    dashboard finished

---
+++ js/main.js
@@ -0,0 +1,80 @@
+$("#dashboard-span").click(() => {
+  $("#dashboard-section").css({ opacity: "1" }, { "z-index": "999" });
+  $("#settings-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#profile-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#projects-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#courses-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+});
+$("#settings-span").click(() => {
+  $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#settings-section").css({ opacity: "1" }, { "z-index": "999" });
+  $("#profile-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#projects-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#courses-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+});
+$("#profile-span").click(() => {
+  $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#settings-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#profile-section").css({ opacity: "1" }, { "z-index": "999" });
+  $("#projects-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#courses-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+});
+$("#projects-span").click(() => {
+  $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#settings-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#profile-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#projects-section").css({ opacity: "1" }, { "z-index": "999" });
+  $("#courses-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+});
+$("#courses-span").click(() => {
+  $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#settings-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#profile-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#projects-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#courses-section").css({ opacity: "1" }, { "z-index": "999" });
+  $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+});
+$("#friends-span").click(() => {
+  $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#settings-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#profile-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#projects-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#courses-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#friends-section").css({ opacity: "1" }, { "z-index": "999" });
+  $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+});
+$("#files-span").click(() => {
+  $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#settings-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#profile-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#projects-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#courses-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#files-section").css({ opacity: "1" }, { "z-index": "999" });
+  $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+});
+$("#plans-span").click(() => {
+  $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#settings-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#profile-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#projects-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#courses-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("#plans-section").css({ opacity: "1" }, { "z-index": "999" });
+});
