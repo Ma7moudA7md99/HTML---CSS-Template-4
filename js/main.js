aa21d84d8b50fe68cd53d5302b6036ca514f78e8
Mahmoud Ahmed <wa7wa776@gmail.com>
2023-02-14 00:28:18 +0200

    dashboard last update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// toggle between sections
 $("#dashboard-span").click(() => {
   $("#dashboard-section").css({ opacity: "1" }, { "z-index": "999" });
   $("#settings-section").css({ opacity: "0" }, { "z-index": "0" });
@@ -7,6 +8,9 @@ $("#dashboard-span").click(() => {
   $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("aside.main").css("left", "-100%");
+  $("i.fa-bars").css("display", "block");
+  $("i.fa-bars-staggered").css("display", "none");
 });
 $("#settings-span").click(() => {
   $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
@@ -17,6 +21,9 @@ $("#settings-span").click(() => {
   $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("aside.main").css("left", "-100%");
+  $("i.fa-bars").css("display", "block");
+  $("i.fa-bars-staggered").css("display", "none");
 });
 $("#profile-span").click(() => {
   $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
@@ -27,6 +34,9 @@ $("#profile-span").click(() => {
   $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("aside.main").css("left", "-100%");
+  $("i.fa-bars").css("display", "block");
+  $("i.fa-bars-staggered").css("display", "none");
 });
 $("#projects-span").click(() => {
   $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
@@ -37,6 +47,9 @@ $("#projects-span").click(() => {
   $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("aside.main").css("left", "-100%");
+  $("i.fa-bars").css("display", "block");
+  $("i.fa-bars-staggered").css("display", "none");
 });
 $("#courses-span").click(() => {
   $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
@@ -47,6 +60,9 @@ $("#courses-span").click(() => {
   $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("aside.main").css("left", "-100%");
+  $("i.fa-bars").css("display", "block");
+  $("i.fa-bars-staggered").css("display", "none");
 });
 $("#friends-span").click(() => {
   $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
@@ -57,6 +73,9 @@ $("#friends-span").click(() => {
   $("#friends-section").css({ opacity: "1" }, { "z-index": "999" });
   $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("aside.main").css("left", "-100%");
+  $("i.fa-bars").css("display", "block");
+  $("i.fa-bars-staggered").css("display", "none");
 });
 $("#files-span").click(() => {
   $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
@@ -67,6 +86,9 @@ $("#files-span").click(() => {
   $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#files-section").css({ opacity: "1" }, { "z-index": "999" });
   $("#plans-section").css({ opacity: "0" }, { "z-index": "0" });
+  $("aside.main").css("left", "-100%");
+  $("i.fa-bars").css("display", "block");
+  $("i.fa-bars-staggered").css("display", "none");
 });
 $("#plans-span").click(() => {
   $("#dashboard-section").css({ opacity: "0" }, { "z-index": "0" });
@@ -77,4 +99,22 @@ $("#plans-span").click(() => {
   $("#friends-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#files-section").css({ opacity: "0" }, { "z-index": "0" });
   $("#plans-section").css({ opacity: "1" }, { "z-index": "999" });
+  $("aside.main").css("left", "-100%");
+  $("i.fa-bars").css("display", "block");
+  $("i.fa-bars-staggered").css("display", "none");
+});
+// toggle search
+$("#search-icon").click(() => {
+  $("#search-input").toggle("display");
+});
+// toggle side bar
+$("i.fa-bars").click(() => {
+  $("aside.main").css("left", "0%");
+  $("i.fa-bars-staggered").css("display", "block");
+  $("i.fa-bars").css("display", "none");
+});
+$("i.fa-bars-staggered").click(() => {
+  $("aside.main").css("left", "-100%");
+  $("i.fa-bars").css("display", "block");
+  $("i.fa-bars-staggered").css("display", "none");
 });
